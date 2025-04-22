da25566fd9340a0b20ca8d19fa045c77b449db03
António Lopes <still_running_uphill@protonmail.com>
2025-04-22 23:46:09 +0100

    Added corrections to main.py and author information to  the READEME

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 from datetime import datetime
 from data_entry import get_date, get_amount, get_category, get_description
+import matplotlib.pyplot as plt
 
 class CSV:
     CSV_FILE = "finance_data.csv"
@@ -64,6 +65,22 @@ def add() -> None:
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
+def plot_transactions(df):
+    df.set_index("date", inplace=True)
+
+    income_df = df[df["category"] == "Income"].resample("D").sum().reindex(df.index, fill_value=0)
+    expense_df = df[df["category"] == "Expense"].resample("D").sum().reindex(df.index, fill_value=0)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(income_df.index, income_df["amount"], label="Income", color="g")
+    plt.plot(expense_df.index, expense_df["amount"], label="Expense", color="r")
+    plt.xlabel("Date")
+    plt.ylabel("Amount")
+    plt.title("Income and Expenses Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show
+
 
 def main() -> None:
     while True:
@@ -78,6 +95,8 @@ def main() -> None:
             start_date= get_date("Enter the start date (dd-mm-yyyy): ")
             end_date = get_date("Enter the end date (dd-mm-yyyy): ")
             df = CSV.get_transactions(start_date, end_date)
+            if input("Do you want to see a plot? (y/n) ").lower() == "y":
+                plot_transactions(df)
         elif choice == "3":
             print("Exiting...")
             break
